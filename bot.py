97e48b751db2724cebed551618d2fed005f0cc0a
domifab <domi.fab98@gmail.com>
2021-03-19 18:44:09 +0100

    add track function to bot.py; change print of vactrack.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
@@ -17,5 +18,11 @@ async def on_ready():
 @client.command(aliases=['Ping', 'Ping!', 'latency', 'ping!'])
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)} ms')
-    
+
+@client.command()
+async def track(ctx, link):
+    p = subprocess.check_output(f'python vactrack.py {link}', text=True, shell=True)
+    await ctx.send(p)
+    print(p)
+
 client.run(TOKEN)
